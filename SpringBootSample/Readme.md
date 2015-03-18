f1ee008de254afa0cc85521e66078339540e3125
Eiji Takahashi <tak7iji@gmail.com>
2015-03-18 17:03:25 +0900

    JMS送受信サンプル（スタンドアローン版）追加

--- SpringBootSample/Readme.md
+++ SpringBootSample/Readme.md
@@ -3,4 +3,5 @@
 * SimpleTest20_2: SimpleTest20_1のWebService版
 * SimpleTest20_3: JMSの送受信サンプル（Tomcat上でHornetQを利用）
 * SimpleTest20_3_1: JMS送受信サンプル（Atomikos利用）
+* SimpleTest20_3_2: JMS送受信サンプル（スタンドアローン版）
 * SimpleTest20_4*: Spring-Securityのサンプル
---
+++ SpringBootSample/SimpleTest20_3_2/src/main/java/org/sample/jboss/jms/JmsClient.java
@@ -0,0 +1,33 @@
+package org.sample.jboss.jms;
+
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class JmsClient implements CommandLineRunner {
+    private static final Logger log = Logger.getLogger(JmsClient.class.getName());
+
+    @Autowired
+    MessageSender messageSender;
+    
+    @Autowired
+    MessageReceiver messageReceiver;
+    
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext ctx = SpringApplication.run(JmsClient.class, args);
+        // アプリケーション終了
+        ctx.close();
+    }
+
+    @Override
+    public void run(String... arg0) throws Exception {
+        log.info("Send message.");
+        messageSender.simpleSend();
+        messageReceiver.receiveMessage();
+    }
+}
